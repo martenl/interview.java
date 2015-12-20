19067f63666928a1aab71a77d7c430a3955118ea
martenl <martenjanlosansky@gmail.com>
2015-12-20 21:10:42 +0100

    Added test for push

---
+++ src/test/java/de/martenl/interview/datastructures/StackTest.java
@@ -0,0 +1,50 @@
+package de.martenl.interview.datastructures;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StackTest {
+
+    private Stack<Integer> objectUnderTest;
+    private LinkedList<Integer> internalData;
+
+    @Before
+    public void setUp() throws Exception {
+        internalData = new LinkedList<>();
+        objectUnderTest = new Stack<>(internalData);
+    }
+
+    @Test
+    public void testCreateStack() throws Exception {
+
+    }
+
+    @Test
+    public void testPush() throws Exception {
+        final int expectedSize = internalData.length()+1;
+        objectUnderTest.push(1);
+        assertEquals(expectedSize,internalData.length());
+    }
+
+    @Test
+    public void testPopWithEmptyStack() throws Exception {
+        final int initialSize = internalData.length();
+        objectUnderTest.pop();
+        assertEquals(initialSize,internalData.length());
+    }
+
+    @Test
+    public void testPop() throws Exception {
+        internalData.add(1,0);
+        final int initialSize = internalData.length();
+        objectUnderTest.pop();
+        assertEquals(initialSize-1,internalData.length());
+    }
+
+    @Test
+    public void testPeek() throws Exception {
+
+    }
+}
