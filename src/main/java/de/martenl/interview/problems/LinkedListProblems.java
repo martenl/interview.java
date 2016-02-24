6b5efa7e1e50382dae5527696600d50808163a29
martenl <martenjanlosansky@gmail.com>
2016-02-24 08:17:34 +0100

    Added class for linked list problems.

---
+++ src/main/java/de/martenl/interview/problems/LinkedListProblems.java
@@ -0,0 +1,12 @@
+package de.martenl.interview.problems;
+
+public class LinkedListProblems {
+    /*
+    In this class are
+    the solutions to the Linked List problems.
+     */
+
+    public void problem1(){
+
+    }
+}
