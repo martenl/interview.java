da6f1f422bf24f3d783388630843e33a75294d29
martenl <martenjanlosansky@gmail.com>
2016-02-24 08:13:48 +0100

    Added methods for index computation.

--- src/main/java/de/martenl/interview/datastructures/BinaryHeap.java
+++ src/main/java/de/martenl/interview/datastructures/BinaryHeap.java
@@ -3,5 +3,19 @@ package de.martenl.interview.datastructures;
 /**
  * Created by Marten on 24.04.2015.
  */
-public class BinaryHeap<T> {
+public class BinaryHeap<T extends Comparable> {
+
+    public static int parent(int index){
+        return index / 2;
+    }
+
+    public static int left(int index){
+        return 2*index;
+    }
+
+    public static int right(int index){
+        return left(index)+1;
+    }
+
+
 }
